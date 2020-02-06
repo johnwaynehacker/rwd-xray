375fff2170e5acb1457b6419b700d17c39d5a964
jpancotti <jpancotti@icloud.com>
2020-02-05 19:51:15 -0500

    fix clarity

--- tools/bin_to_rwd.py
+++ tools/bin_to_rwd.py
@@ -82,9 +82,11 @@ car_models = {
   },
 
    '39990-TRW-A020': { #clarity thanks to wirelessnet2
+     'can-address': '0x18DA30F1',
      'supported-versions': ['39990-TRW-A010', '39990-TRW-A020', '39990-TRW,A010', '39990-TRW,A020'],
      'security-key': ['0x011101121120', '0x011101121120', '0x011101121120', '0x011101121120'],
      'encryption-key': '0x010203',
+     'start-address': 0x4000,
      'data-size': 0x4c000,
       #(checksum func idx, offset)
      'checksum-offsets': [(0, 0x4bf80), (1, 0x4bffe)]
